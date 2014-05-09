6d14ea9d0d96c5ff97e61755619fb459248ee32d
Tokerliang <woshiliangkai@gmail.com>
2014-05-09 16:04:56 +0800

    Update conn.php

--- messages/conn.php
+++ messages/conn.php
@@ -1,22 +1,17 @@
 <?php
 
-/*
- * PHP100Job v1.0
- * Programmer : Msn/QQ haowubai@hotmail.com (925939)
- * www.php100.com Develop a project PHP - MySQL - Apache
- * Window 2003 - Preferences - PHPeclipse - PHP - Code Templates
- */
 
-$conn = @ mysql_connect("localhost", "root", "") or die("Êý¾Ý¿âÁ´½Ó´íÎó");
+
+$conn = @ mysql_connect("localhost", "root", "") or die("ï¿½ï¿½ï¿½Ý¿ï¿½ï¿½ï¿½ï¿½Ó´ï¿½ï¿½ï¿½");
 mysql_select_db("bbs", $conn);
-mysql_query("set names 'GBK'"); //Ê¹ÓÃGBKÖÐÎÄ±àÂë;
+mysql_query("set names 'GBK'"); //Ê¹ï¿½ï¿½GBKï¿½ï¿½ï¿½Ä±ï¿½ï¿½ï¿½;
 
 function htmtocode($content) {
 	$content = str_replace("\n", "<br>", str_replace(" ", "&nbsp;", $content));
 	return $content;
 }
 
-//$content=str_replace("'","¡®",$content);
+//$content=str_replace("'","ï¿½ï¿½",$content);
  //htmlspecialchars();
 
 
